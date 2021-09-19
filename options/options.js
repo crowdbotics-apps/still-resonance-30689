97be9ec59f85e416bf740d97c719617db3d840b3
Crowdbotics <team@crowdbotics.com>
2021-09-19 23:39:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "still_resonance_30689Identifier",
+  url: "https://still_resonance_30689Identifier.botics.co",
+  api: "https://still_resonance_30689Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
